d065d08868c876f76da1d128e99af306065f561f
Lucas Tymen <lucas.tymen@gmail.com>
2023-02-14 10:35:02 +0100

    mealMaker_done

--- MealMaker.js
+++ MealMaker.js
@@ -1,21 +1,31 @@
 const menu = {
-  _meal: (''),
+  _meal: "",
   _price: 0,
   set meal(mealToCheck) {
-  if (typeof mealToCheck === 'string') {
-    return this._meal = mealToCheck;
-  } else {
-    console.log('please, write a menu composant');
-  }
-},
-set price(priceToCheck) {
-  if (typeof priceToCheck === 'number') {
-    return this._price = priceToCheck;
+    if (typeof mealToCheck === "string") {
+      return (this._meal = mealToCheck);
     } else {
-      console.log('please, write number');
+      console.log("please, write a menu composant");
     }
-}
+  },
+  set price(priceToCheck) {
+    if (typeof priceToCheck === "number") {
+      return (this._price = priceToCheck);
+    } else {
+      console.log("please, write number");
+    }
+  },
+  get todaySpecial() {
+    if (this._meal && this._price) {
+      return `Today’s Special meal is ${this._meal} for ${this._price} €!`;
+    } else {
+      return "Meal or price was not set correctly!";
+    }
+  },
 };
+menu.meal = "menu big mac veggie";
+menu.price = 12;
 
+console.log(menu.todaySpecial);
 
-console.log(menu);
+// console.log(menu);
